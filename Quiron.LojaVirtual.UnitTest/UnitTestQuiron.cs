be33cc210df0ca1f0fc455ef2a4885fc97ebe650
wilhans1984 <wilhans2007@hotmail.com>
2017-10-23 09:18:49 -0200

    View controller

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Quiron.LojaVirtual.UnitTest
@@ -7,8 +8,29 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTestQuiron
     {
         [TestMethod]
-        public void TestMethod1()
+        [Ignore]
+        public void Take()
         {
+            //O operador Take é usado para selecionar os primeiros objetos da colecao
+            int[] numeros = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] teste = {5, 4, 1, 3, 9};
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
+        [TestMethod]
+        public void Skip()
+        {
+            //O operador skip ignora os primeiros n objetos de uma coleção
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = { 1, 3, 9};
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
     }
 }
