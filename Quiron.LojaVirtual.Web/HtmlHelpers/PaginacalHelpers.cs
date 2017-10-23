e9b0d2933fed79ad348aa3827911c993d1bfff67
wilhans1984 <wilhans2007@hotmail.com>
2017-10-23 11:08:45 -0200

    Classes de paginacao

---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacalHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacalHelpers
+    {
+
+        //Total de paginas = 3
+        public static MvcHtmlString PAgeLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPaginas; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
