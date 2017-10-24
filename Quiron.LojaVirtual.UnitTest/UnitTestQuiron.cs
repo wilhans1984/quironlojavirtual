ec0e349c9b3e391681d107046d9c26e96dda3018
wilhans1984 <wilhans2007@hotmail.com>
2017-10-24 11:19:09 -0200

    Teste Paginacao

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -8,7 +11,7 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTestQuiron
     {
         [TestMethod]
-        [Ignore]
+        
         public void Take()
         {
             //O operador Take é usado para selecionar os primeiros objetos da colecao
@@ -32,5 +35,28 @@ namespace Quiron.LojaVirtual.UnitTest
 
             CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
+        [TestMethod]
+        public void TestarPaginacao()
+        {
+            //Arrange
+
+            HtmlHelper html = null;
+            Paginacao paginacao = new Paginacao {PaginaAtual = 2, ItensTotal = 28, ItensPorPagina = 10};
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+            MvcHtmlString resultado = html.PAgeLinks(paginacao, paginaUrl);
+
+            //Assert
+            Assert.AreEqual(
+                    @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                    + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                    + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
+
+
+        }
     }
+
+
 }
--- Quiron.LojaVirtual.Web/HtmlHelpers/PaginacalHelpers.cs
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacalHelpers.cs
@@ -13,7 +13,7 @@ namespace Quiron.LojaVirtual.Web.HtmlHelpers
         {
             StringBuilder resultado = new StringBuilder();
 
-            for (int i = 1; i < paginacao.TotalPaginas; i++)
+            for (int i = 1; i <= paginacao.TotalPaginas; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", paginaUrl(i));
